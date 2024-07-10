43153d697720d89aadccd03211d1b149e4e1c7f1
Jacob <jacobrashid404@gmail.com>
2024-07-10 12:17:37 -0700

    sumValues, countEvens implemented

--- trees/count-evens/countEvens.ts
+++ trees/count-evens/countEvens.ts
@@ -4,7 +4,22 @@ import { TreeNodeNum } from "../common/tree";
  * children, count how many nodes have even values. Returns that count as
  * an integer. */
 function countEvens(node: TreeNodeNum): number {
-  return 42;
+  let evens = 0;
+  const nodes: TreeNodeNum[] = [node];
+
+  while (nodes.length !== 0) {
+    const current = nodes.pop()!;
+
+    if (current.val % 2 === 0) {
+      evens++;
+    }
+
+    for (const child of current.children) {
+      nodes.push(child);
+    }
+  }
+
+  return evens;
 }
 
 export { countEvens };
--- trees/sum-values/sumValues.ts
+++ trees/sum-values/sumValues.ts
@@ -3,7 +3,20 @@ import { TreeNodeNum } from "../common/tree";
 /** sumValues(): add up all values of node and its descendants.
  * Returns sum as an integer. */
 function sumValues(node: TreeNodeNum): number {
-  return 42;
+  let sum = 0;
+  const nodes: TreeNodeNum[] = [node];
+
+  while (nodes.length !== 0) {
+    const current = nodes.pop()!;
+
+    sum += current.val;
+
+    for (const child of current.children) {
+      nodes.push(child);
+    }
+  }
+
+  return sum;
 }
 
 export { sumValues };
