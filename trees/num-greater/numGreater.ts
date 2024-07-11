a72f0c117d6ea1bcc0d228b3905c25825c39ec1d
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-07-10 20:14:50 -0400

    Implement numGreater with code that passes tests

--- trees/num-greater/numGreater.ts
+++ trees/num-greater/numGreater.ts
@@ -1,11 +1,25 @@
-import { TreeNodeNum } from "../common/tree";
+import { TreeNode, TreeNodeNum } from "../common/tree";
 
 /** numGreater(lowerBound): starting from the invoking node and moving
  * through its children, return a count of the number of nodes whose value
  * is greater than lowerBound. */
 
 function numGreater(node: TreeNodeNum, lowerBound: number): number {
-  return 42;
+  let count = 0;
+
+  const nodes: TreeNodeNum[] = [node];
+
+  while (nodes.length !== 0) {
+    const currentNode = nodes.pop()!;
+
+    if (currentNode.val > lowerBound) count++;
+
+    for (const child of currentNode.children) {
+      nodes.push(child);
+    }
+  }
+
+  return count;
 }
 
 export { numGreater };
