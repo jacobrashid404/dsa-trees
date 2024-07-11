d92a8ecb6449d0b695e4c0e30cb5492e18efd902
Jacob <jacobrashid404@gmail.com>
2024-07-10 18:14:41 -0700

    pre and post order implemented

--- trees/pre-post-order/prePostOrder.ts
+++ trees/pre-post-order/prePostOrder.ts
@@ -4,15 +4,36 @@ import { TreeNodeNum } from "../common/tree";
  * Returns an array of values of visited nodes. */
 
 function preOrder(node: TreeNodeNum | null): number[] {
-  return [42];
-}
+  if (node === null) return [];
+  let nodeVals: number[] = [];
+  nodeVals.push(node.val);
 
+  for(const child of node.children) {
+    const childVals = preOrder(child);
+    nodeVals.push(...childVals);
+  }
+
+  return nodeVals;
+}
+// 1
+//  \
+//   2
+// [1, 2]
 
 /** dfsPostOrder(): Traverse from the invoking node using post-order DFS.
  * Returns an array of values of visited nodes. */
 
 function postOrder(node: TreeNodeNum | null): number[] {
-  return [42];
+  if (node === null) return [];
+  let nodeVals: number[] = [];
+
+  for(const child of node.children) {
+    const childVals = postOrder(child);
+    nodeVals.push(...childVals);
+  }
+  nodeVals.push(node.val);
+
+  return nodeVals;
 }
 
 export { preOrder, postOrder };
